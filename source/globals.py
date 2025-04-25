c199086aaad182a2874218c00617c9046af126c1
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-04-25 23:02:05 +0200

    add tries number in json config

--- source/globals.py
+++ source/globals.py
@@ -4,8 +4,12 @@ from GameMode import GameMode
 
 mode : GameMode = GameMode.World
 
-game : FlagleGame = FlagleGame(5)
+number_of_tries = 5
 
 with open("config.json", "r") as file:
 
 	data = json.load(file)
+
+	number_of_tries = data["number_of_tries"]
+
+game : FlagleGame = FlagleGame(number_of_tries)

a58bf1041c9404a32fd78e5e845de5f144524f3f
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-04-26 00:40:42 +0200

    working on image

--- source/image_utils.py
+++ source/image_utils.py
@@ -16,18 +16,8 @@ def get_image_aspect_ratio(image: Image) -> float:
 	return (image.width / image.height)
 
 
-def get_standardized_image(path: string) -> Image:
+def get_standardized_image(path: string, width: int = 1920, height: int = 1080) -> Image:
 
-	image : Image = Image.open(path)
+	image: Image = Image.open(path)
 
-	width, height = image.size
-	image_aspect_ratio = width / height
-
-	if image_aspect_ratio > standard_aspect_ratio:
-		new_width = int(standard_aspect_ratio * height)
-		new_height = height
-	else:
-		new_width = width
-		new_height = int(width / standard_aspect_ratio)
-
-	return (image.resize((new_width, new_height), Image.Resampling.LANCZOS))
+	return (image.resize((width, height), Image.Resampling.LANCZOS))

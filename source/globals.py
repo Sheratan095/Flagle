75392fd4bdfe7d9d5d502ec1bbc2125cb387969d
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-05-05 16:49:37 +0200

    globals are not in config.json

--- source/globals.py
+++ source/globals.py
@@ -1,40 +1,7 @@
 import json
-import string
-import os  # Import os to work with absolute paths
+import os
 from Game import FlagleGame
 from GameMode import GameMode
-
-# BACKEND
-mode: GameMode = GameMode.World
-
-max_tries: int = 6
-
-testing: bool = False
-testing_nation: str = "Nepal"
-trasparent: tuple = (0, 0, 0, 0)
-
-color_tollerance: int = 40
-
-max_width: int = 0
-max_height: int = 0
-
-
-# FRONTEND
-main_screen_idx: int = 1
-background_color: string = "#1D2951"
-border_color: string = "#8A8A8A"
-input_color: string = "#FFFFFF"
-img_width_GUI: int = 300
-img_height_GUI: int = 200
-max_lenght_txtbox: int = 25
-txtbox_placeholder: string = "Guess the country"
-txtbox_placeholder_color: string = "gray"
-
-txtbox_img_width: int = 38
-txtbox_img_height: int = 25
-main_img_width: int = 247
-main_img_height: int = 165
-main_img_background: string = "#243364"
 
 # Get the absolute path of the project directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,13 +10,37 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 config_path = os.path.join(BASE_DIR, "config.json")
 with open(config_path, "r") as file:
 	data = json.load(file)
-	max_tries = data["number_of_tries"]
 
+# BACKEND
+mode: GameMode = GameMode.World
+max_tries: int = data["number_of_tries"]
+color_tollerance: int = data["backend"]["color_tollerance"]
+testing: bool = data["backend"]["testing"]
+testing_nation: str = data["backend"]["testing_nation"]
+trasparent: tuple = tuple(data["backend"]["trasparent"])
+
+# FRONTEND
+main_screen_idx: int = data["frontend"]["main_screen_idx"]
+background_color: str = data["frontend"]["background_color"]
+border_color: str = data["frontend"]["border_color"]
+input_color: str = data["frontend"]["input_color"]
+img_width_GUI: int = data["frontend"]["img_width_GUI"]
+img_height_GUI: int = data["frontend"]["img_height_GUI"]
+max_lenght_txtbox: int = data["frontend"]["max_lenght_txtbox"]
+txtbox_placeholder: str = data["frontend"]["txtbox_placeholder"]
+txtbox_placeholder_color: str = data["frontend"]["txtbox_placeholder_color"]
+txtbox_img_width: int = data["frontend"]["txtbox_img_width"]
+txtbox_img_height: int = data["frontend"]["txtbox_img_height"]
+main_img_width: int = data["frontend"]["main_img_width"]
+main_img_height: int = data["frontend"]["main_img_height"]
+main_img_background: str = data["frontend"]["main_img_background"]
+
+# Initialize the game
 game: FlagleGame = FlagleGame(max_tries)
 
 
 def get_fixed_country_name(country_name: str, max_length: int) -> str:
-	
+
 	fixed_name = country_name
 
 	if (len(country_name) > max_length):
